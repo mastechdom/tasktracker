7a9d65a0810a8a0e5c279f985d9ab24b0366f4d0
Mohd Azhar <mohd.azhar@madgicaltechdom.com>
2021-09-20 14:23:58 +0530

    first

---
+++ src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="abt">
+    <h3>Version 1.0.0</h3>
+    <router-link to="/">Go Back</router-link>
+</div>
+</template>
+
+
+
+
+
+<style scoped>
+a {
+    color: #333;
+
+}
+.abt {
+    margin-top: 30px;
+    text-align: center;
+}
+</style>
